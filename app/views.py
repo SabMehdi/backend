7c182019ae019a25d5e40c0fa015f79ace4676ad
saber <106769715+SabMehdi@users.noreply.github.com>
2023-10-04 21:49:07 +0200

    commentaires

--- app/views.py
+++ app/views.py
@@ -3,23 +3,30 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from nltk.tokenize import word_tokenize
 import nltk
+
+#bibliotheque de tokenization
 nltk.download('punkt')
+#ignorer les mesures de securité csrf
 @csrf_exempt
 def process_text(request):
     if request.method == 'POST':
+
+        #lecture des données depuis la méthode POST en assument que le fichier=file
         uploaded_file = request.FILES['file']
+        
+        #transformation du binaire vers string
         content = uploaded_file.read().decode('utf-8')
         
-        # Tokenize the text content
+        # transformation du texte vers des tokens (tokenize the text), le resultat est une liste de mots
         tokens = word_tokenize(content)
         
-        # Create an inverted index
-        inverted_index = {}
+        # creation du inverted index
+        inverted_index = {} #dictionnaire
         for idx, token in enumerate(tokens):
             if token not in inverted_index:
                 inverted_index[token] = [idx]
             else:
                 inverted_index[token].append(idx)
 
-        # Serialize and return the inverted index
+        # Serialisation et renvoie de inverted_index
         return JsonResponse({'inverted_index': inverted_index})
