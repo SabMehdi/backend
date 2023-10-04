c040c650a404ab37b09a5f785d43ccb37d84e39c
saber <106769715+SabMehdi@users.noreply.github.com>
2023-10-04 20:41:03 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from nltk.tokenize import word_tokenize
+
+@csrf_exempt
+def process_text(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['file']
+        content = uploaded_file.read().decode('utf-8')
+        
+        # Tokenize the text content
+        tokens = word_tokenize(content)
+        
+        # Create an inverted index
+        inverted_index = {}
+        for idx, token in enumerate(tokens):
+            if token not in inverted_index:
+                inverted_index[token] = [idx]
+            else:
+                inverted_index[token].append(idx)
+
+        # Serialize and return the inverted index
+        return JsonResponse({'inverted_index': inverted_index})
