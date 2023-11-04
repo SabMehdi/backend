821ee1283d582a0e62a873c3f4cf9250bd4de845
saber <106769715+SabMehdi@users.noreply.github.com>
2023-11-04 23:34:58 +0100

    cloud

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import json
 import string
+import unidecode
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from nltk.tokenize import word_tokenize
@@ -24,56 +25,54 @@ nlp = spacy.load('fr_core_news_md')
 @csrf_exempt
 def process_text(request):
     if request.method == 'POST':
-
         uploaded_file = request.FILES['file']
-
         content = uploaded_file.read().decode('utf-8')
 
-        token_pattern =r"\w+(?:-\w+)*|\."
-
-        
-        tokens = list(enumerate(re.findall(token_pattern, content)))
-        #print(tokens)
-
-        lemmas = {}
-       # content = content.translate(str.maketrans('', '', string.punctuation))
-
-        with open('app\stop_words_french.json', 'r') as json_file:
+        with open('app/stop_words_french.json', 'r') as json_file:
             stp_words = set(json.load(json_file))
+            stp_words = set(unidecode.unidecode(word).lower() for word in stp_words)
+        print(f"Loaded stopwords: {stp_words}")  # Debugging line
+
+        tokens = content.split()
         inverted_index = {}
-        for idx, (position, token) in enumerate(tokens):
-            token=token.lower()
-            if token not in stp_words and token not in ['»', '«', 'à'] and token not in string.punctuation and token.isalpha():
-                doc = nlp(token)
-                for word in doc:
-                    if(not word.pos_=='VERB'):
-                        lemmas[word.text] = word.lemma_
-                for token, lemma in lemmas.items():
-                    #print(f"{token} : {lemma}")
+
+        for position, token in enumerate(tokens):
+            cleaned_token = token.lower().strip(string.punctuation)
+            unaccented_token = unidecode.unidecode(cleaned_token)  # Normalize for comparison
+
+            if unaccented_token in stp_words:
+                print(f"Filtered out: {cleaned_token}")  # Debugging line
+            elif cleaned_token.isalpha():
+                doc = nlp(cleaned_token)
+                lemma = [word.lemma_ for word in doc if not word.pos_ == 'VERB']
+
+                if lemma:
+                    lemma = lemma[0]
                     if lemma not in inverted_index:
                         inverted_index[lemma] = [position]
                     else:
                         inverted_index[lemma].append(position)
-            lemmas.clear() 
+
         file_analysis = FileAnalysis(file_path=uploaded_file, inverted_index=inverted_index)
-        file_analysis.save()          
+        file_analysis.save()
+
         return JsonResponse({'inverted_index': inverted_index})
 
 def index(request):
     return HttpResponse("Welcome to My Django App")
 
+
 @csrf_exempt
 def get_file_names(request):
     file_names = [result.file_path for result in FileAnalysis.objects.all()]
-    print(file_names)
     return JsonResponse(file_names, safe=False)
-    
+
+
 @csrf_exempt
 def get_inverted_index(request, file_name):
     try:
         lemmatization_result = FileAnalysis.objects.get(file_path=file_name)
         inverted_index = lemmatization_result.inverted_index  # Access the correct attribute
-        print(inverted_index)  # This line is for debugging
         return JsonResponse(inverted_index, safe=False)
     except FileAnalysis.DoesNotExist:
         return JsonResponse({'error': 'File not found'}, status=404)
